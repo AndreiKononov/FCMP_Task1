cfc33d83cb69dbd55034e5239a43283bab6826b9
Andrei Konanau <Andrei_Konanau@epam.com>
2018-11-14 11:19:38 +0300

    Last updates

--- src/main.js
+++ src/main.js
@@ -10,18 +10,18 @@ const container = document.getElementById('container');
 fetch(req)
   .then(response => response.json())
   .then(response => {
-    for(let i = 0; i < response.articles.length; i++ ){
+    response.articles.forEach((article) => {
       let dataItemEl = document.createElement('article');
       dataItemEl.className = 'item';
       dataItemEl.innerHTML = `
-        <span>${response.articles[i].source.name} / ${response.articles[i].publishedAt}</span> 
+        <span>${article.source.name} / ${article.publishedAt}</span> 
         <header>
-          <h1><a class="item-title" target="_blank" href="${response.articles[i].url}">${response.articles[i].title}</a></h1>
+          <h1><a class="item-title" target="_blank" href="${article.url}">${article.title}</a></h1>
         </header>        
-        <p>${response.articles[i].description}</p>
-        <img src="${response.articles[i].urlToImage}" alt="${response.articles[i].title}">
+        <p>${article.description}</p>
+        <img src="${article.urlToImage}" alt="${article.title}">
       `;
       container.appendChild(dataItemEl);
-    }
+    })
   })
   .catch(error => console.log(error));
