c45f9c3d085d1b5d9a0e62acb06711073a8946ae
Andrei Konanau <Andrei_Konanau@epam.com>
2018-12-10 18:10:34 +0300

    Small updates

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,18 +1,19 @@
 import { fragment, container, loader} from "./constants";
 import { articleTemplate } from "./template";
 
-function showArticle(data) {
-  loader.classList.add('hidden');
-  const {articles = []} = data;
-  articles.forEach(article => {
-    let dataItemEl = document.createElement('article');
-    dataItemEl.className = 'item';
-    dataItemEl.innerHTML = articleTemplate(article);
-    fragment.appendChild(dataItemEl);
-  });
-  container.appendChild(fragment);
-}
-
-export const CONTROLLER = (() => ({
-  showArticle
-}))();
+export const  CONTROLLER = (function () {
+  const showArticle = function (data) {
+    loader.classList.add('hidden');
+    const {articles = []} = data;
+    articles.forEach(article => {
+      let dataItemEl = document.createElement('article');
+      dataItemEl.className = 'item';
+      dataItemEl.innerHTML = articleTemplate(article);
+      fragment.appendChild(dataItemEl);
+    });
+    container.appendChild(fragment);
+  };
+  return {
+    showArticle: showArticle
+  }
+})();
