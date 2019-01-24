2192243e123559d808c68b58f42064ca7868d99b
Andrei Kononov <andrei.kononov85@gmail.com>
2019-01-25 01:08:45 +0300

    The first update

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.use((err, req, res) => {
+    res.status(500);
+    res.render('error', { error: err });
+});
+
+let port = +(process.env.PORT || 4000);
+
+app.listen(port, () => console.log(`Express server working now on http://localhost:${port}`));
